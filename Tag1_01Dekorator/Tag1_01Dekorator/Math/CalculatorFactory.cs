4f6d6ffc37c697b93d25611166f78b9bf0cee9f4
Jo Wagner <jowagner@limago.de>
2024-10-10 10:55:10 +0200

    Dekorator mit Factory

---
+++ Tag1_01Dekorator/Tag1_01Dekorator/Math/CalculatorFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tag1_01Dekorator.Math
+{
+    public class CalculatorFactory
+    {
+
+        public static ICalculator createInstance() { 
+            return new Calculator(); 
+        }
+    }
+}
