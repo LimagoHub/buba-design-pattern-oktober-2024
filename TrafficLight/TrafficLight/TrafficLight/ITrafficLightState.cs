c3d5cfe0e36c5286d4b38a40b7948802522db577
Jo Wagner <jowagner@limago.de>
2024-10-10 15:48:28 +0200

    stateuebung

---
+++ TrafficLight/TrafficLight/TrafficLight/ITrafficLightState.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficLightProject.TrafficLight
+{
+    internal interface ITrafficLightState
+    {
+        string GetColor();
+        void NextColor();
+    }
+}
